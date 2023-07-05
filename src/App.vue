347d0eef3507d08b53c6ff5f65224bbeb0e1204f
ardiansyah999 <131391384+ardiansyah999@users.noreply.github.com>
2023-07-05 12:58:29 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/">Cuaca</router-link></li>
+          <li><router-link to="/location">Location</router-link></li>
+          <li><router-link to="/stopwatch">Stopwatch</router-link></li>
+          <li><router-link to="/tictactoe">Tic Tac Toe</router-link></li>
+          <li><router-link to="/photo">Photo</router-link></li>
+          <li><router-link to="/widget">About Me</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+background-image: url(https://wallpapercave.com/wp/ZYOP3Ym.jpg);
+}
+#app {
+  text-align: center;
+}
+
+header {
+  
+  background: #d81414;
+  padding: 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+}
+
+nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+nav li {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+nav a:hover {
+  background-color: #19e623;
+}
+</style>
---
+++ src/components/widget.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="widget">
+      <h2>{{ title }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: 'Widget Title',
+        message: 'Hello, Saya Muhammad Ardiansyah, 213510595. ini merupakan project sederhana saya mengenai widget',
+        
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .widget {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin-bottom: 20px;
+  }
+  </style>
+  
